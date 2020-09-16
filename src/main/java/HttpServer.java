a5b73168259435c2778b1e8874858f81ef040434
SebEil <56046884+SebEil@users.noreply.github.com>
2020-09-16 19:02:25 +0200

    Initial commit

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+    public HttpServer(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        //While loop så siden kan lastes inn på nytt
+        while (true){
+        new Thread(() -> {
+            try {
+                Socket socket = serverSocket.accept();
+                handleRequest(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new HttpServer(8080);
+    }
+
+    private static void handleRequest(Socket socket) throws IOException {
+        String responseCode = "200";
+
+        String requestLine = HttpClient.readLine(socket);
+        System.out.println(requestLine);
+        String requestTarget = requestLine.split(" ")[1];
+        int questionPos = requestTarget.indexOf('?');
+        if (questionPos != -1) {
+            String queryString = requestTarget.substring(questionPos+1);
+            int equalPos = queryString.indexOf('=');
+            String parameterValue = queryString.substring(equalPos+1);
+            responseCode = parameterValue;
+        }
+
+
+        String response = "HTTP/1.1 " + responseCode + " OK\r\n" +
+                "Content-Type: text/html; charset=utf-8\r\n" +
+                "Content-Length: 11\r\n" +
+                "\r\n" +
+                "Hello sever";
+
+        socket.getOutputStream().write(response.getBytes());
+    }
+}
---
+++ src/test/java/HTTPClientTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HTTPClientTest {
+
+    @Test
+    void shouldReturnSuccessfulStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80,"/echo");
+        assertEquals(200, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnErrorfulStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80,"/echo?status=404");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReadResponseHeader() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80,"/echo?body=Kristiania");
+        assertEquals("10", client.getResponseHeader("Content-Length"));
+    }
+
+    @Test
+    void shouldReadResponseBody() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80,"/echo?body=Kristiania");
+        assertEquals("Kristiania", client.getResponseBody());
+    }
+
+}
